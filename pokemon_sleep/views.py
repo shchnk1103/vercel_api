1f04caa44370a2bfeb2666d8d89182660f66a590
shchnk <761962425@qq.com>
2023-08-21 16:27:17 +0800

    add pokemon search

--- pokemon_sleep/views.py
+++ pokemon_sleep/views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from pokemon_sleep.Serializers import PokemonSerializer, PokemonCharactorSerializer, PokemonSecondarySkillSerializer
-from pokemon_sleep.Utils.pandas import parse_secondary_skill
+from pokemon_sleep.Utils.pandas import parse_secondary_skill, parse_excel, parse_character
 from pokemon_sleep.Utils.spiders import start_spider
 from pokemon_sleep.models import pokemon_collection, pokemon_character_collection, pokemon_secondary_skill_collection
 
 
 class PokemonSleepViewSet(viewsets.ViewSet):
     pagination_class = PageNumberPagination
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
 
     def list(self, request):
         paginator = self.pagination_class()
@@ -20,7 +21,15 @@ class PokemonSleepViewSet(viewsets.ViewSet):
         page_number = request.query_params.get('page', 1)  # ?page=1
         skip = (int(page_number) - 1) * page_size
 
-        pokemons = pokemon_collection.find().skip(skip).limit(page_size)
+        # 获取过滤参数
+        name_filter = request.query_params.get('name')
+
+        # 构建查询条件
+        query = {}
+        if name_filter:
+            query['name'] = {'$regex': name_filter, '$options': 'i'}  # i 代表忽略大小写
+
+        pokemons = pokemon_collection.find(query).skip(skip).limit(page_size)
 
         serializer = PokemonSerializer(pokemons, many=True)
         return Response(serializer.data)
